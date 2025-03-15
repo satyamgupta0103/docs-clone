83d3e84de664ca867821f5854b977a8a376e39ef
Satyam <74068263+satyamgupta0103@users.noreply.github.com>
2025-03-15 23:33:50 +0530

    TextEditor.tsx: Added funtionality to Listen for Local text changes and save it to Firebase

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,8 +1,35 @@
-import { useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import ReactQuill from "react-quill";
+import { db } from "../firebase-config";
+import { doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
+import "react-quill/dist/quill.snow.css";
+import { throttle } from "lodash";
+import "../App.css";
 
 export const TextEditor = () => {
   const quillRef = useRef<any>(null);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const saveContent = () => {};
+
+  useEffect(() => {
+    if (quillRef.current) {
+      //Load initial content from Firestore
+
+      //Listen to Firestore for any updates and update locally in real-time
+
+      //Listen for Local text changes and save it to Firebase
+      const editor = quillRef.current.getEditor(); // creating a quill instance
+      editor.on("text-change", () => {
+        setIsEditing(true);
+        saveContent();
+
+        setTimeout(() => {
+          setIsEditing(false);
+        }, 5000);
+      });
+    }
+  }, []);
 
   return (
     <div className="google-docs-editor">
